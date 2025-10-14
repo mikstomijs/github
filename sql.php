25001fc771fd436f6342789e4457309af15f69db
miks <mikstomijs@gmail.com>
2025-10-14 14:21:11 +0300

    first commit

---
+++ sql.php
@@ -0,0 +1,33 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "veikals_db";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+
+
+<?php
+$sql = "SELECT * FROM logininfo";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
---
+++ sql.sql
@@ -0,0 +1,4 @@
+INSERT INTO logininfo (name, description, price, stock, image_url)
+VALUES ('iPhone 15', 'Latest Apple smartphone', 999.99, 50, 'iphone15.jpg'),
+       ('Galaxy S24', 'Samsung flagship phone', 899.99, 30,  'galaxys24.jpg');
+   
